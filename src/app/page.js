f07eef3948344673ca73aece85206983404c9871
Sem Gabriel Roig <sem.roig@gmail.com>
2023-10-31 23:39:19 -0300

    agrego container

--- src/app/page.js
+++ src/app/page.js
@@ -51,75 +51,82 @@ export default function Home() {
   }
 
   return (
-    <Center>
-      <Stack spacing={1} direction="column">
-        <Stack spacing={1} direction="row">
-          <Stack spacing={1} direction="column">
-            <Heading>Hola! Somos Sem y Vicky</Heading>
-            <Text fontSize="2xl">
-              In love with React & Next. In love with React & Next. In love with
-              React & Next. In love with React & Next. In love with React &
-              Next.
-            </Text>
-          </Stack>
-          <div>imagen</div>
-        </Stack>
+    <Container maxW="6xl" my={20}>
+      <Stack direction="row">
+        <Box>
+          <Heading>Hola! Somos Sem y Vicky</Heading>
+          <Text fontSize="2xl">
+            In love with React & Next. In love with React & Next. In love with
+            React & Next. In love with React & Next. In love with React & Next.
+          </Text>
+        </Box>
 
-        <Stack spacing={1} direction="row">
-          <Card size="sm">
-            <CardBody>
-              <Image
-                src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-                alt="Green double couch with wooden legs"
-                borderRadius="lg"
-                boxSize="200px"
-              />
-              <Heading size="md"> Customer dashboard</Heading>
-            </CardBody>
-            <CardFooter>
-              <Button variant="solid" colorScheme="blue">
-                Agregar al carrito
-              </Button>
-            </CardFooter>
-          </Card>
-          <Card size="sm">
-            <CardBody>
-              <Image
-                src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-                alt="Green double couch with wooden legs"
-                borderRadius="lg"
-                boxSize="200px"
-              />
-              <Heading size="md"> Customer dashboard</Heading>
-            </CardBody>
-            <CardFooter>
-              <Button variant="solid" colorScheme="blue">
-                Agregar al carrito
-              </Button>
-            </CardFooter>
-          </Card>
-          <Card size="sm">
-            <CardBody>
-              <Image
-                src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-                alt="Green double couch with wooden legs"
-                borderRadius="lg"
-                boxSize="200px"
-              />
-              <Heading size="md"> Customer dashboard</Heading>
-            </CardBody>
-            <CardFooter>
-              <Button variant="solid" colorScheme="blue">
-                Agregar al carrito
-              </Button>
-            </CardFooter>
-          </Card>
-        </Stack>
-
-        <Button onClick={onOpen}>
-          Ver carrito <Icon as={BsCart2} ml={1} />
-        </Button>
+        <Image
+          src="https://bit.ly/dan-abramov"
+          alt="Dan Abramov"
+          boxSize="400px"
+        />
       </Stack>
+
+      <Heading>Hola! Somos Sem y Vicky</Heading>
+      <Text fontSize="2xl">
+        In love with React & Next. In love with React & Next.
+      </Text>
+
+      <Stack direction="row">
+        <Card size="sm">
+          <CardBody>
+            <Image
+              src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+              alt="Green double couch with wooden legs"
+              borderRadius="lg"
+              boxSize="200px"
+            />
+            <Heading size="md"> Customer dashboard</Heading>
+          </CardBody>
+          <CardFooter>
+            <Button variant="solid" colorScheme="blue">
+              Agregar al carrito
+            </Button>
+          </CardFooter>
+        </Card>
+        <Card size="sm">
+          <CardBody>
+            <Image
+              src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+              alt="Green double couch with wooden legs"
+              borderRadius="lg"
+              boxSize="200px"
+            />
+            <Heading size="md"> Customer dashboard</Heading>
+          </CardBody>
+          <CardFooter>
+            <Button variant="solid" colorScheme="blue">
+              Agregar al carrito
+            </Button>
+          </CardFooter>
+        </Card>
+        <Card size="sm">
+          <CardBody>
+            <Image
+              src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+              alt="Green double couch with wooden legs"
+              borderRadius="lg"
+              boxSize="200px"
+            />
+            <Heading size="md"> Customer dashboard</Heading>
+          </CardBody>
+          <CardFooter>
+            <Button variant="solid" colorScheme="blue">
+              Agregar al carrito
+            </Button>
+          </CardFooter>
+        </Card>
+      </Stack>
+
+      <Button onClick={onOpen}>
+        Ver carrito <Icon as={BsCart2} ml={1} />
+      </Button>
 
       <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
@@ -141,6 +148,6 @@ export default function Home() {
           </ModalFooter>
         </ModalContent>
       </Modal>
-    </Center>
+    </Container>
   );
 }
